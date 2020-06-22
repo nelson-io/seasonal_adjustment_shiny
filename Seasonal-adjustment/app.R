fec0d026089b3bc38148a5828725994615bc3756
Nelson Shilman <nelsonshilman@gmail.com>
2020-06-22 01:29:26 -0300

    center image and set beta for publication

--- Seasonal-adjustment/app.R
+++ Seasonal-adjustment/app.R
@@ -29,19 +29,19 @@ ui <- fluidPage(
                          h4(strong("Input file")),
                          p("Choose a file from your system with Time Series data. \n The first column ", strong("MUST"), " contain the date of each observation while the next columns ", 
                            strong("MUST"), "contain the values associated to those dates like in the following table:"),
-                         img(src = "https://github.com/nelson-io/seasonal_adjustment_shiny/raw/master/table.PNG"),
+                         img(src = "https://github.com/nelson-io/seasonal_adjustment_shiny/raw/master/table.PNG", style="display: block; margin-left: auto; margin-right: auto;"),
                          h4(strong("Data consumption")),
                          p( "Press the download button in order to download the adjusted data or see the results at the 'Seasonal Adjusted table' tab. \n Have in mind that depending on the size of the dataset it may take some time to process and adjust every series."),
                          h4(strong("Considerations")),
                          p(code("Statistical method"),"- In order to adjust the time series, the ", strong("X13-ARIMA-SEATS")," method is used. Next updates will include the possibility to use the ", strong("X11"), " method."),
-                         p(code("Time Series Legth"), "- Data must contain at least 3 years of observations in order to apply seasonal adjusting methods"),
-                         p(code("NAs"), "- Data must be complete. Any single NA would invalidate the data so they must be treated before using this App. (Next updates will include time-series extrapolation methods to solve this issue)"),
-                         p(code("frequency"), "- This App uses an algorithm that automatically infers the correct frequency of your data so it`s unnecessary to specificate it manually"),
+                         p(code("Time Series Legth"), "- Data must contain at least 3 years of observations in order to apply seasonal adjusting methods."),
+                         p(code("NAs"), "- Data must be complete. Any single NA would invalidate the data so they must be treated before using this App. (Next updates will include time-series extrapolation methods to solve this issue)."),
+                         p(code("frequency"), "- This App uses an algorithm that automatically infers the correct frequency of your data so it`s unnecessary to specificate it manually."),
                          p(code("date structure"), "- This App uses an algorithm that can parse almost any virtually existing date or datetime structure automatically!"),
                          p(""),
                          p(""),
                          p(""),
-                         p(a("Here", href= "https://github.com/nelson-io/seasonal_adjustment_shiny/raw/master/emae-apertura-por-sectores-valores-mensuales-indice-base-2004.csv"), " is a sample .xlsx file to try the app!")
+                         p(a("Here", href= "https://github.com/nelson-io/seasonal_adjustment_shiny/raw/master/sample_data_emae.xlsx"), " is a sample .xlsx file to try the app!")
                 ),
                 tabPanel("Seasonal Adjusted table", DT::dataTableOutput("seas_table"))
             )
